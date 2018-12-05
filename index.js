24ca1042664c8231c024fd21620d9a2a1426c4b4
Maykel Fleitas <m.fleitas@live.com>
2018-12-04 20:17:37 -0500

    Changed port number to 4000

--- index.js
+++ index.js
@@ -120,8 +120,8 @@ app.get('/year', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log('Server listening on port 5000');
+    console.log('Server listening on port 4000');
 });
